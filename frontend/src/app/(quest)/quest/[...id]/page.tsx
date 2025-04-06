965395167e96ef2fd6e4f06258f1c55a32bd78d9
sam <sanskarsharma9005@gmail.com>
2025-04-06 23:53:41 +0530

    fix redirection to game

--- frontend/src/app/(quest)/quest/[...id]/page.tsx
+++ frontend/src/app/(quest)/quest/[...id]/page.tsx
@@ -5,8 +5,8 @@ export default function IndividualQuest({
 }: {
   params: { id: string }
 }) {
-  const {id} = params;
+  const { id } = params;
   console.log(`id for the code ${id}`);
-  if (id === "1") redirect("https://quant.edaquest.com/")
+  if (id == "1") redirect("https://quant.edaquest.com/")
   else return <>something went wrong!</>
 }
