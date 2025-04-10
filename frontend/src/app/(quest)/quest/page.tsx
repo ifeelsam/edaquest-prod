a9826181758ab1303f05bbe1987faa3bf683d88b
sam <sanskarsharma9005@gmail.com>
2025-04-10 21:55:41 +0530

    deployment fix

--- frontend/src/app/(quest)/quest/page.tsx
+++ frontend/src/app/(quest)/quest/page.tsx
@@ -1,12 +1,9 @@
-"use client"
-import TempGame from '@/components/game/unityGame';
 
 
 export default function GamePage() {
   return (
     <div>
       <h1>My Unity Game</h1>
-      <TempGame/>
     </div>
   );
 }
